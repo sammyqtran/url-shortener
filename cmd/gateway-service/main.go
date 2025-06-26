2ecb5ec2060590fb2279c33010d7b47396f39e01
sammyqtran <sammyqtran@gmail.com>
2025-06-26 11:52:42 -0700

    Implemented basic gateway, updated docker compose and created dockerfile for gateway

---
+++ cmd/gateway-service/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sammyqtran/url-shortener/internal/gateway"
+	pb "github.com/sammyqtran/url-shortener/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+
+	conn, err := grpc.NewClient("url-service:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	grpcClient := pb.NewURLServiceClient(conn)
+
+	server := &gateway.GatewayServer{
+		GrpcClient: grpcClient,
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/create", server.HandleCreateShortURL).Methods("POST")
+	r.HandleFunc("/{shortCode}", server.HandleGetOriginalURL).Methods("GET")
+	r.HandleFunc("/healthz", server.HandleHealthCheck).Methods("GET")
+
+	log.Println("Gateway service listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
